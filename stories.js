97b0e544048b67ac2f7701b7bb681b50af78e8d9
jearnold2 <jearnold2@liberty.edu>
2018-08-06 11:44:24 -0400

    fixed missing commas

--- stories.js
+++ stories.js
@@ -6,271 +6,271 @@ var stories = [
         "date": "2015"
     },
     {
-        "title": "BekaH.Alcoholism&Addiction&Codependency.2014.mp3"
+        "title": "BekaH.Alcoholism&Addiction&Codependency.2014.mp3",
         "name": "Beka H",
         "subject": "Alcoholism, Addiction, Codependency",
         "date": "2014"
     },
     {
-        "title": "BekaH.ChemicalAddiction.2016.mp3"
+        "title": "BekaH.ChemicalAddiction.2016.mp3",
         "name": "Beka H",
         "subject": "Chemical Addiction",
         "date": "2016"
     },
     {
-        "title": "Brandon&Heather.Addiction&Codependency.2014.mp3"
+        "title": "Brandon&Heather.Addiction&Codependency.2014.mp3",
         "name": "Brandon & Heather",
         "subject": "Addiction, Codependency",
         "date": "2014"
     },
     {
-        "title": "ChristieA.Grief&Codependency.2015.mp3"
+        "title": "ChristieA.Grief&Codependency.2015.mp3",
         "name": "Christie A",
         "subject": "Grief, Codependency",
         "date": "2015"
     },
     {
-        "title": "Dean.Codependency&ACFD.2016.mp3"
+        "title": "Dean.Codependency&ACFD.2016.mp3",
         "name": "Dean",
         "subject": "Codependency, ACFD",
         "date": "2016"
     },
     {
-        "title": "DebbieH.Alcoholism&ControlIssues.2015.mp3"
+        "title": "DebbieH.Alcoholism&ControlIssues.2015.mp3",
         "name": "Debbie H",
         "subject": "Alcoholism, Control Issues",
         "date": "2015"
     },
     {
-        "title": "DebbieO.Codependency.2016.mp3"
+        "title": "DebbieO.Codependency.2016.mp3",
         "name": "Debbie O",
         "subject": "Codependency",
         "date": "2016"
     },
     {
-        "title": "DebiM.Abuse&SexualImpurity&perfectionism.2018.mp3"
+        "title": "DebiM.Abuse&SexualImpurity&perfectionism.2018.mp3",
         "name": "Debi M",
         "subject": "Abuse, Sexual Impurity, Perfectionism",
         "date": "2018"
     },
     {
-        "title": "EricS.SexualIntegrity.2016.mp3"
+        "title": "EricS.SexualIntegrity.2016.mp3",
         "name": "Eric S",
         "subject": "Sexual Integrity",
         "date": "2016"
     },
     {
-        "title": "GloriaL.Acohol&Drugs&Codependency.2015.mp3"
+        "title": "GloriaL.Acohol&Drugs&Codependency.2015.mp3",
         "name": "Gloria L",
         "subject": "Alcohol, Drugs, Codependency",
         "date": "2015"
     },
     {
-        "title": "HelenD.Codependency.2015.mp3"
+        "title": "HelenD.Codependency.2015.mp3",
         "name": "Helen D",
         "subject": "Codependency",
         "date": "2015"
     },
     {
-        "title": "JohnE.Codependency.2015.mp3"
+        "title": "JohnE.Codependency.2015.mp3",
         "name": "John E",
         "subject": "Codependency",
         "date": "2015"
     },
     {
-        "title": "JulieJ.Depression&Codepedency&SelfEsteem&Fear.2017.mp3"
+        "title": "JulieJ.Depression&Codepedency&SelfEsteem&Fear.2017.mp3",
         "name": "Julie J",
         "subject": "Depression, Codependency, Self-Esteem, Fear",
         "date": "2017"
     },
     {
-        "title": "KimA.ACFD.2015.mp3"
+        "title": "KimA.ACFD.2015.mp3",
         "name": "Kim A",
         "subject": "ACFD",
         "date": "2015"
     },
     {
-        "title": "LauraE.ACFD.2015.mp3"
+        "title": "LauraE.ACFD.2015.mp3",
         "name": "Laura E",
         "subject": "ACFD",
         "date": "2015"
     },
     {
-        "title": "LauraT.SexualAbuse&Integrity.2016.mp3"
+        "title": "LauraT.SexualAbuse&Integrity.2016.mp3",
         "name": "Laura T",
         "subject": "Sexual Abuse, Integrity",
         "date": "2016"
     },
     {
-        "title": "LindseyM.Alcoholism.2016.mp3"
+        "title": "LindseyM.Alcoholism.2016.mp3",
         "name": "Lindsey M",
         "subject": "Alcoholism",
         "date": "2016"
     },
     {
-        "title": "LindseyM.CA&Codependency.2015.mp3"
+        "title": "LindseyM.CA&Codependency.2015.mp3",
         "name": "Lindsey M",
         "subject": "CA, Codependency",
         "date": "2015"
     },
     {
-        "title": "LisaH.Codepedency.2015.mp3"
+        "title": "LisaH.Codepedency.2015.mp3",
         "name": "Lisa H",
         "subject": "Codependency",
         "date": "2015"
     },
     {
-        "title": "LisaH.seeprevious.2017.mp3"
+        "title": "LisaH.seeprevious.2017.mp3",
         "name": "Lisa H",
         "subject": "Codependency 2",
         "date": "2017"
     },
     {
-        "title": "LisaK.ACFD&Addiction.2016.mp3"
+        "title": "LisaK.ACFD&Addiction.2016.mp3",
         "name": "Lisa K",
         "subject": "ACFD, Addiction",
         "date": "2016"
     },
     {
-        "title": "LouE.Addiction.2015.mp3"
+        "title": "LouE.Addiction.2015.mp3",
         "name": "Lou E",
         "subject": "Addiction",
         "date": "2015"
     },
     {
-        "title": "Michael&Dana.Drugs Alcohol&Codependency Control.2017.mp3"
+        "title": "Michael&Dana.Drugs Alcohol&Codependency Control.2017.mp3",
         "name": "Michael & Dana",
         "subject": "Drugs, Alcohol, Codependency, Control",
         "date": "2017"
     },
     {
-        "title": "MikeM.Codependency.2016.mp3"
+        "title": "MikeM.Codependency.2016.mp3",
         "name": "Mike M",
         "subject": "Codependency",
         "date": "2016"
     },
     {
-        "title": "MindyE.Anger.2017.mp3"
+        "title": "MindyE.Anger.2017.mp3",
         "name": "Mindy E",
         "subject": "Anger",
         "date": "2017"
     },
     {
-        "title": "NatashaH.ACFD.MP3"
+        "title": "NatashaH.ACFD.MP3",
         "name": "Natasha H",
         "subject": "ACFD",
         "date": "None"
     },
     {
-        "title": "NicoleH.Alcohol.2015.mp3"
+        "title": "NicoleH.Alcohol.2015.mp3",
         "name": "Nicole H",
         "subject": "Alcohol",
         "date": "2015"
     },
     {
-        "title": "NicoleH.ChemicalAddiction.2016.mp3"
+        "title": "NicoleH.ChemicalAddiction.2016.mp3",
         "name": "Nicole H",
         "subject": "Chemical Addiction",
         "date": "2016"
     },
     {
-        "title": "Rich.SelfEsteem&Anger&Fear&Addiction&Molestation.2018.mp3"
+        "title": "Rich.SelfEsteem&Anger&Fear&Addiction&Molestation.2018.mp3",
         "name": "Rich",
         "subject": "Self-Esteem, Anger, Fear, Addiction, Molestation",
         "date": "2018"
     },
     {
-        "title": "Sean&Shannon.Anger&Finances&Abuse&Alcohol Drugs&Rape.2017.mp3"
+        "title": "Sean&Shannon.Anger&Finances&Abuse&Alcohol Drugs&Rape.2017.mp3",
         "name": "Sean & Shannon",
         "subject": "Anger, Finances, Abuse, Alcohol, Drugs, Rape",
         "date": "2017"
     },
     {
-        "title": "Sean&Shannon.Seeprevious.2015.mp3"
+        "title": "Sean&Shannon.Seeprevious.2015.mp3",
         "name": "Sean & Shannon",
         "subject": "See Previous",
         "date": "2015"
     },
     {
-        "title": "SeanM.Anger&Finances.2016.mp3"
+        "title": "SeanM.Anger&Finances.2016.mp3",
         "name": "Sean M",
         "subject": "Anger, Finances",
         "date": "2016"
     },
     {
-        "title": "SharonD.Drugs&SexualAbuse.2013.mp3"
+        "title": "SharonD.Drugs&SexualAbuse.2013.mp3",
         "name": "Sharon D",
         "subject": "Drugs, Sexual Abuse",
         "date": "2013"
     },
     {
-        "title": "Sherrice.FearTrust&Anger.2017.mp3"
+        "title": "Sherrice.FearTrust&Anger.2017.mp3",
         "name": "Sherrice",
         "subject": "Fear, Trust, Anger",
         "date": "2017"
     },
     {
-        "title": "StephanieI.Alcohol&Addiction.2016.mp3"
+        "title": "StephanieI.Alcohol&Addiction.2016.mp3",
         "name": "Stephanie I",
         "subject": "Alcohol, Addiction",
         "date": "2016"
     },
     {
-        "title": "Steve&Tami.Anger&SelfHatred.2017.mp3"
+        "title": "Steve&Tami.Anger&SelfHatred.2017.mp3",
         "name": "Steve & Tami",
         "subject": "Anger, Self-Hatred",
         "date": "2017"
     },
     {
-        "title": "TomK.SexualPurity.2016.mp3"
+        "title": "TomK.SexualPurity.2016.mp3",
         "name": "Tom K",
         "subject": "Sexual Purity",
         "date": "2016"
     },
     {
-        "title": "ToniD.Codependency&SexualAssault.2016.mp3"
+        "title": "ToniD.Codependency&SexualAssault.2016.mp3",
         "name": "Toni D",
         "subject": "Codependency, Sexual Assault",
         "date": "2016"
     },
     {
-        "title": "TonyE.Addiction&Insecurity.2013.mp3"
+        "title": "TonyE.Addiction&Insecurity.2013.mp3",
         "name": "Tony E",
         "subject": "Addiction, Insecurity",
         "date": "2013"
     },
     {
-        "title": "TonyE.Seeprevious.2016.mp3"
+        "title": "TonyE.Seeprevious.2016.mp3",
         "name": "Tony E",
         "subject": "See Previous",
         "date": "2016"
     },
     {
-        "title": "VicB.Anger.2016.mp3"
+        "title": "VicB.Anger.2016.mp3",
         "name": "Vic B",
         "subject": "Anger",
         "date": "2016"
     },
     {
-        "title": "Wade.SSAttraction&Depression&Anxiety.2017.mp3"
+        "title": "Wade.SSAttraction&Depression&Anxiety.2017.mp3",
         "name": "Wade",
         "subject": "Attraction, Depression, Anxiety",
         "date": "2017"
     },
     {
-        "title": "Wendy.ChemicalAddiction.2013.mp3"
+        "title": "Wendy.ChemicalAddiction.2013.mp3",
         "name": "Wendy",
         "subject": "Chemical Addiction",
         "date": "2013"
     },
     {
-        "title": "Weston.Insecurity&SexualAssault&Manipulation.2014.mp3"
+        "title": "Weston.Insecurity&SexualAssault&Manipulation.2014.mp3",
         "name": "Weston",
         "subject": "Insecurity, Sexual Assault, Manipulation",
         "date": "2014"
     },
     {
-        "title": "Weston.Seeprevious.2015.mp3"
+        "title": "Weston.Seeprevious.2015.mp3",
         "name": "Weston",
         "subject": "See Previous",
         "date": "2015"
