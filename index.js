8f4cd021b1222eb103402f9232adc655e476917a
Tony Erskine <tony@sharptop.co>
2018-08-08 16:31:04 -0400

    major refactor

--- index.js
+++ index.js
@@ -1,39 +1,73 @@
-const s3URL = 'https://s3.amazonaws.com/crstories.org/';
+// Constants
+const s3BaseURL = 'https://s3.amazonaws.com/crstories.org/';
 const windowURL = window.location.href.split('?')[0];
-var story = atob(getFromParameterOrLocalStorage('story'));
-var password = getParameterByName('password');
-console.log(story);
-console.log(windowURL);
-
-function storyTemplate(item, url, i, shareURL = null, password = null) {
-    let line1 = '<h1>' + item.name + ' - ' + item.date + '</h1>'
-    let line2 = '<h4>' + item.subject + '</h4>'
-    let line3 = ''
-      line3 = '<p><a href onclick="copyShareLink(\'' + shareURL + '\')"><span class="button">Share this Story</span></a></p>';
-      line4 = '<p><a href="' + url + '"><span id="listen-now" class="button">' + 'Listen Now' + '</span></a></p>';
-    let line5 = '<div id="qrcode' + i + '" class="qrcode"></div>';
-    let lines = '<div class="story"><div class="description">' + line1 + line2 + line3 + line4 + '</div><div class="qr">' + line5 + '</div></div>';
-    return lines;
-}
+const storyParam = atob(getFromParameterOrLocalStorage('storyParam'));
+const passwordParam = getParameterByName('password');
+const password = '1MomentStrongSitUnder';
 
 function copyShareLink(shareURL) {
-  window.prompt("Copy the following link for a sharable page", shareURL);
+    window.prompt("Copy the following link for a sharable page", shareURL);
 }
 
-function addListItems() {
+function generateQRCode(qrElementId, sharingURL) {
+    let qrcode = new QRCode(qrElementId);
+    qrcode.makeCode(sharingURL);
+}
+
+function formatMetadata(story) {
+    return `
+      <h1>${story.name} - ${story.date}</h1>
+      <h4>${story.subject}</h4>
+    `;
+}
+
+function formatButtons(shareURL, url) {
+    return `
+        <div>
+            <a href onclick="copyShareLink('${shareURL}')"><span class="button">Share this Story</span></a>
+            <a href="${url}"><span id="listen-now" class="button">Listen Now</span></a>
+        </div>
+    `;
+}
+
+function formatQRCodeDiv(i) {
+    return `<div id="qrcode${i}" class="qrcode"></div>`;
+}
+
+function formatStory(storyElements, buttonsElements, qrCodeElements) {
+    let template = `
+        <div class="story">
+            <div class="description">
+                ${storyElements}
+                ${buttonsElements}
+            </div>
+            ${qrCodeElements}
+        </div>
+    `;
+    return template;
+}
+
+function displayStory(story, storyNumber) {
+    let mp3URL = s3BaseURL + story.title;
+    let sharingURL = windowURL + '?storyParam=' + btoa(story.title);
+    $("#stories").append(formatStory(formatMetadata(story), formatButtons(sharingURL, mp3URL), formatQRCodeDiv(storyNumber)));
+    generateQRCode("qrcode" + storyNumber, sharingURL);
+}
+
+function shouldDisplayStory(story) {
+    return storyParam === story.title || passwordParam === password;
+}
+
+function displayStories() {
     let i = 0;
-    stories.forEach(item => {
-        let storyURL = s3URL + item.title;
-        let sharingURL = windowURL + '?story=' + btoa(item.title);
-        if(story === item.title || password === '1MomentStrongSitUnder') {
-            $("#stories").append(storyTemplate(item, storyURL, i, sharingURL, password));
-            let qrcode = new QRCode("qrcode" + i);
-            qrcode.makeCode(sharingURL);
+    stories.forEach(story => {
+        if(shouldDisplayStory(story)) {
+            displayStory(story, i);
             i++;
         }
     })
 }
 
 $(document).ready(function(){
-    addListItems();
+    displayStories();
 });
